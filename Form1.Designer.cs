102534b803acf75c5815e09d34aa73ad2fdb4e0f
Danil <danit2609@mail.ru>
2024-05-04 20:44:20 +0300

    Добавили вторую кнопку

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,36 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(275, 165);
+            button1.Location = new Point(88, 92);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(224, 83);
+            button1.Size = new Size(196, 62);
             button1.TabIndex = 0;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(401, 142);
+            button2.Name = "button2";
+            button2.Size = new Size(191, 81);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -54,5 +67,6 @@
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
